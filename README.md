a9cd3cfed18cc82cef863d18a7ef318339475f29
adil MOUHSSINE <adil.mouhssine@gmail.com>
2018-04-19 17:42:42 +0200

    start project for money account management

---
+++ README.md
@@ -0,0 +1,13 @@
+Account Management
+==================
+
+Application to help to manage correctly your money
+
+  <a href="https://github.com/vchaptsev/cookiecutter-django-vue">
+      <img src="https://img.shields.io/badge/built%20with-Cookiecutter%20Django%20Vue-blue.svg" />
+  </a>
+
+  
+  ## License
+  MIT
+  
---
+++ account_management/static/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+
+import axios from 'axios'
+import router from './router'
+import {store} from './store'
+import Meta from 'vue-meta'
+import VueAnalytics from 'vue-analytics'
+
+
+
+
+import Main from './Main.vue'
+
+// Axios csrf settings
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+
+
+
+// more info: https://github.com/MatteoGabriele/vue-analytics
+Vue.use(VueAnalytics, {id: GOOGLE_ANALYTICS, router})
+
+
+
+
+
+Vue.use(Meta)
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#main',
+  router,
+  store,
+  render: h => h(Main)
+})
